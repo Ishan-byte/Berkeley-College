412d0db1275913e797475b258c7a0247a7d375c1
SamratGhale <Samrat.biz19@gmail.com>
2022-03-22 11:55:59 +0545

    Charts

--- 19031002 Ishan chemjong_Code/Berkeley_College-master/Controllers/HomeController.cs
+++ 19031002 Ishan chemjong_Code/Berkeley_College-master/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using berkeley_college.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,14 +15,69 @@ namespace berkeley_college.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ModelContext _context;
+
+        public HomeController(ModelContext context, ILogger<HomeController> logger)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            List<DataPoint> dataPoints = new List<DataPoint>();
+
+            var modules = await _context.Module.FromSqlRaw($"select * from module").ToListAsync();
+
+            foreach(var module in modules){
+                int x = (await _context.ModuleStudent.FromSqlRaw($"select * from module_student where module_id = '{module.ModuleId}'").ToListAsync()).Count;
+                dataPoints.Add(new DataPoint { X = x, Y = module.ModuleName});
+            }
+
+            ViewBag.ChartTitle = "Number of student in on each module";
+            ViewBag.ChartSubTitle = "";
+            ViewBag.ChartType = "pie";
+            String j = JsonConvert.SerializeObject(dataPoints);
+			ViewBag.DataPoints = j;
+
+
+
+            //Chart 2
+
+            List<DataPoint> dataPoints2 = new List<DataPoint>();
+
+            string[] ts = new string[4] { "A", "B", "C","D"};
+
+            for (int i = 0; i < 4; i++)
+            {
+                string y = ts[i] ;
+                int x = (await _context.ResultInfo.FromSqlRaw($"select * from result_info where grade = '{ts[i]}'").ToListAsync()).Count;
+                dataPoints2.Add(new DataPoint { X = x, Y = y });
+            }
+
+            ViewBag.ChartTitle2 = "Number of students in each grade";
+            ViewBag.ChartSubTitle2 = "X = grade, Y = number of students with that grade";
+            ViewBag.ChartType2 = "area";
+            String j2 = JsonConvert.SerializeObject(dataPoints2);
+			ViewBag.DataPoints2 = j2;
+
+            
+            //Chart 3
+            List<DataPoint> dataPoints3 = new List<DataPoint>();
+
+            foreach(var module in modules){
+                int x = (await _context.StudentAttendenceInfo.FromSqlRaw($"select * from student_attendence_info where module_id = '{module.ModuleId}'").ToListAsync()).Count;
+                dataPoints3.Add(new DataPoint { X = x, Y = module.ModuleName});
+            }
+
+            ViewBag.ChartTitle3 = "Number of attendance in each Module";
+            ViewBag.ChartSubTitle3 = "X = name of module , Y = number of attendance in the module";
+            ViewBag.ChartType3 = "area";
+            String j3 = JsonConvert.SerializeObject(dataPoints3);
+			ViewBag.DataPoints3 = j3;
+
+
+            return View("~/Views/Charts/Index.cshtml");
         }
 
         public IActionResult Privacy()
